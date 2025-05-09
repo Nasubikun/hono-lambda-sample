a85f38d70a12ae546fa66b48450c0b49e0483b0a
Jo Matsuda <jomatsu@amazon.co.jp>
2025-05-09 14:44:48 +0900

    add s3 handler

--- lambda/app.ts
+++ lambda/app.ts
@@ -1,5 +1,27 @@
+import { S3Event } from 'aws-lambda';
 import { Hono } from 'hono'
+import { LambdaContext, LambdaEvent } from 'hono/aws-lambda';
 
-export const app = new Hono()
+type Bindings = {
+    event: LambdaEvent;
+    lambdaContext: LambdaContext;
+  };
 
-app.get('/', (c) => c.text('Hello Hono!'))
+export const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', (c) => {
+    if ('Records' in c.env.event) {
+        console.log('AWS TRIGGER EVENT - audio uploaded S3: ', c.env.event);
+        const s3Event = c.env.event as S3Event;
+        const bucket = s3Event.Records[0].s3.bucket.name;
+        const objectKey = decodeURIComponent(
+          s3Event.Records[0].s3.object.key.replace(/\+/g, ' '),
+        );
+
+        console.log('Bucket: ', bucket);
+        console.log('Object Key: ', objectKey);
+
+        return c.json({ message: 'Done handling new s3 event' }, 200);
+    }
+    return c.text('Hello Hono!');
+})
--- lib/hono-lambda-sample-stack.ts
+++ lib/hono-lambda-sample-stack.ts
@@ -2,21 +2,40 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications'
 
 export class HonoLambdaSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const fn = new NodejsFunction(this, 'lambda', {
-      entry: 'lambda/index.ts',
+    const bucket = new s3.Bucket(this, 'UploadBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    })
+
+    const fn = new NodejsFunction(this, 'LambdaHandler', {
+      entry: 'lambda/handler.ts',
       handler: 'handler',
       runtime: lambda.Runtime.NODEJS_22_X,
     })
+
+    bucket.grantReadWrite(fn)
+
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      new s3n.LambdaDestination(fn)
+    )
+
     const fnUrl = fn.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.AWS_IAM,
     })
+
     new cdk.CfnOutput(this, 'lambdaUrl', {
-      value: fnUrl.url!
+      value: fnUrl.url!,
+    })
+    new cdk.CfnOutput(this, 'bucketName', {
+      value: bucket.bucketName,
     })
   }
 }
